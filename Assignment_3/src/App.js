683c53267002f2e0596d778f725ed15626640172
nk-neha <115693111+nk-neha@users.noreply.github.com>
2024-04-12 00:05:45 +0530

    Add files via upload

---
+++ Assignment_3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ParentComponent from './ParentComponent';
+
+function App() {
+  return (
+    <div className="app">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
